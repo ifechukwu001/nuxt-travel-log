437673cc5e0ad1b87d3651a038286d17403f53a4
Ogidi Ifechukwu <ogidiifechukwu6@gmail.com>
2026-08-24 19:03:52 +0100

    :sparkles: Add ssr to authentication (#11)

---
+++ app/composables/auth.ts
@@ -0,0 +1,12 @@
+import { createAuthClient } from "better-auth/vue";
+
+export function useAuth() {
+  const url = useRequestURL();
+
+  const headers = import.meta.server ? useRequestHeaders(["cookie"]) : undefined;
+
+  return createAuthClient({
+    baseURL: url.origin,
+    fetchOptions: { headers },
+  });
+}
