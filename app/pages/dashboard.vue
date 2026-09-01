770245352ac9b4c52fd43ce9f71d31d7020a182d
Ogidi Ifechukwu <ogidiifechukwu6@gmail.com>
2026-09-01 12:13:59 +0100

    :sparkles: View all my locations in an interactive map (#18)

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -19,7 +19,7 @@ function toggleSidebar() {
 
 <template>
   <div class="flex-1 flex">
-    <div class="bg-base-100 transition-all duration-300" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+    <div class="bg-base-100 transition-all duration-300 shrink-0" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
       <div class="flex hover:cursor-pointer hover:bg-base-200 p-2" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }" @click="toggleSidebar">
         <Icon
           v-if="isSidebarOpen"
@@ -69,9 +69,11 @@ function toggleSidebar() {
         />
       </div>
     </div>
-    <div class="flex-1 flex flex-col">
-      <NuxtPage />
-      <AppMap class="flex-1" />
+    <div class="flex-1 overflow-auto">
+      <div class="flex flex-col size-full">
+        <NuxtPage />
+        <AppMap class="flex-1" />
+      </div>
     </div>
   </div>
 </template>
